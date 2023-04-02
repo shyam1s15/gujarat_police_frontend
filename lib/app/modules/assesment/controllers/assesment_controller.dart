4086418c8720b79ad12a2abebeaff3b08df6ecd2
shyam kachhadiya <shyam1ss15@gmail.com>
2023-04-02 21:26:49 +0000

    kas dkas d as dk

--- lib/app/modules/assesment/controllers/assesment_controller.dart
+++ lib/app/modules/assesment/controllers/assesment_controller.dart
@@ -1,7 +1,9 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:e_bandobas/app/jsondata/EventData/Event.dart';
 import 'package:e_bandobas/app/jsondata/EventData/EventApi.dart';
 import '../../../../constants/enums.dart';
+import '../../../Exceptions/ValidationException.dart';
 import '../../../jsondata/EventPoliceCount/EventPoliceCountAPI.dart';
 import '../../../jsondata/EventPoliceCount/EventPoliceCountOfAssignedTotalRequestedModel.dart';
 import '../../event/views/event_view.dart';
@@ -9,7 +11,8 @@ import '../../event/views/event_view.dart';
 class AssesmentController extends GetxController {
   late final selectedEventId = 0.obs;
   final events = Rxn<List<Event>>();
-  final eventAssignmentCounts = Rxn<List<EventPoliceCountAssignedTotalRequestedModel>>();
+  final eventAssignmentCounts =
+      Rxn<List<EventPoliceCountAssignedTotalRequestedModel>>();
   final eventDataSource = Rxn<EventViewDataGridSource>();
 
   final count = 0.obs;
@@ -36,7 +39,8 @@ class AssesmentController extends GetxController {
 
   void getEventAssignments() async {
     eventAssignmentCounts.value =
-    await EventPoliceCountAPI.obtainEventPoliceCountAssignments(API_Decision.Only_Failure, selectedEventId.value);
+        await EventPoliceCountAPI.obtainEventPoliceCountAssignments(
+            API_Decision.Only_Failure, selectedEventId.value);
   }
 
   @override
@@ -44,10 +48,24 @@ class AssesmentController extends GetxController {
     super.onInit();
     loadEvents();
   }
-  
+
   Future<EventViewDataGridSource> getEventViewDataGridSource() async {
     List<Event>? eventList = await events();
     return EventViewDataGridSource(eventList!);
   }
 
+  Future<bool> saveEvent(String eventName, String eventDetails,
+      DateTime startDate, DateTime endDate) async {
+    bool result = false;
+    if (eventName.isNotEmpty) {
+      result = await EventApi.createEvent(API_Decision.Only_Success, eventName,
+          eventDetails, startDate, endDate);
+      loadEvents();
+      update();
+      return true;
+    } else {
+      ValidationException().showValidationSnackBar();
+    }
+    return false;
+  }
 }
